5fa1ae5614e308c9e51ca48f3dca896683af045a
Andrea Grillo <andrea.grillo96@live.com>
2023-07-07 10:25:15 +0200

    create vm

--- main.tf
+++ main.tf
@@ -1,11 +1,51 @@
+locals {
+  zone = "us-central1-c"
+}
+
 provider "google" {
   credentials = var.sa
 
   project = var.project_id
   region  = "us-central1"
-  zone    = "us-central1-c"
+  zone    = local.zone
 }
 
 resource "google_compute_network" "vpc_network" {
-  name = "terraform-network"
+  name = "krateo-network"
+}
+
+resource "google_compute_instance" "default" {
+  name         = "krateo-compute"
+  machine_type = "e2-medium"
+  zone         = local.zone
+
+  tags = ["foo", "bar"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      labels = {
+        my_label = "value"
+      }
+    }
+  }
+
+  // Local SSD disk
+  scratch_disk {
+    interface = "SCSI"
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
+  metadata = {
+    foo = "bar"
+  }
+
+  metadata_startup_script = "echo hi > /test.txt"
 }
